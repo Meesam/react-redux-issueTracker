052d235f88e9371e704e7259ade56a8869366b1d
meesam <meesam.engineer@gmail.com>
2017-04-13 23:40:26 +0530

    add delete option in comment

--- public/src/components/issueComments.jsx
+++ public/src/components/issueComments.jsx
@@ -3,23 +3,50 @@ import { reduxForm, Field, SubmissionError,initialize } from 'redux-form';
 import renderTextArea from '../common/renderTextArea.jsx';
 import renderField from '../common/renderField.jsx';
 
+const styles={
+  onEnter:{
+    display:'block'
+  },
+  onLeave:{
+    display:'none'
+  }
+}
+
 class IssueComment extends Component{
   constructor(props){
     super(props)
+    this.onmouserEnter=this.onmouserEnter.bind(this);
+    this.onmouserLeave=this.onmouserLeave.bind(this);
+    this.state={
+      isMouseEnter:false
+    }
+  }
+
+  onmouserLeave(){
+    this.setState({
+      isMouseEnter:false
+    });
+  }
+
+  onmouserEnter(){
+    this.setState({
+      isMouseEnter:true
+    });
   }
 
   renderComment(comments){
     return comments.map((item)=>{
       return(
-        <div class='box-footer box-comments' key={item._id}>
+        <div class='box-footer box-comments' key={item._id} onMouseEnter={this.onmouserEnter} onMouseLeave={this.onmouserLeave}>
           <div class='box-comment'>
             <img class='img-circle img-sm' src='styles/img/user1-128x128.jpg' alt='user image' />
             <div class='comment-text'>
               <span class="username">
                 {item.CommentBy}
-                <span class='text-muted pull-right'>8:03 PM Today</span>
+                <span class='text-muted pull-right' style={this.state.isMouseEnter ? styles.onEnter : styles.onLeave}><a href="#"><i class="fa fa-close"></i></a></span>
               </span>
-              {item.CommentText}
+              <span class='text-muted'>8:03 PM Today</span>
+             <div>{item.CommentText}</div> 
             </div>
           </div>
         </div>
