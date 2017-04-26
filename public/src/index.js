13cc1d64deef38868c42fb340a2363d269610a9a
meesam <meesam.engineer@gmail.com>
2017-04-26 21:42:43 +0530

    no changes

--- public/src/index.js
+++ public/src/index.js
@@ -19,6 +19,7 @@ import Authenticated from './containers/authenticationContainer.jsx';
 import AppModuleIndex from  './pages/appModulesIndex.jsx';
 import IssueIndex from './pages/issueIndex.jsx';
 import NewIssueIndex from './pages/newIssueIndex.jsx';
+import ChartsIndex from './pages/chartIndex.jsx';
 
 
 //for React Developer Tools
@@ -39,6 +40,7 @@ render(
         <Route path="issues" components={IssueIndex}></Route>
         <Route path="newissue" components={NewIssueIndex}></Route>
         <Route path="editissue/:Id" components={NewIssueIndex}></Route>
+        <Route path="charts" components={ChartsIndex}></Route>
         <Route path="*" component={NotFoundPage}/>
       </Route>  
     </Router>
